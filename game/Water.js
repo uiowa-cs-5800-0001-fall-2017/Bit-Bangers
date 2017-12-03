c1fc29d923d1b0d36bde22e81373565b65cdcc28
Jonathan White <jonathan-white@uiowa.edu>
2017-12-03 00:22:00 +0000

    working water map

--- game/Water.js
+++ game/Water.js
@@ -98,8 +98,8 @@ function preload() {
   game.load.spritesheet('coin', 'https://res.cloudinary.com/harsay/image/upload/v1464614984/coin_iormvy.png', 16, 16);
   game.load.spritesheet('instruct', 'img/level1_Instructions.png', 255, 255);
 
-  game.load.tilemap('level1', 'img/level1single.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('tiles1', 'img/level1_tiles.png'); //load tileset corresponding level1single.json tilemap
+  game.load.tilemap('Water_Map', 'img/Water_Map.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('wtiles', 'img/Water_Tiles.png'); //load tileset corresponding level1single.json tilemap
 }
 
 function create() {
@@ -111,12 +111,13 @@ function create() {
 
   game.stage.backgroundColor = '#363f44';
 
-  map = game.add.tilemap('level1');
-  map.addTilesetImage('level1_tiles', 'tiles1');
+  map = game.add.tilemap('Water_Map');
+  map.addTilesetImage('Water_Tiles', 'wtiles');
   map.setCollisionBetween(0, 10000, true, 'Tile Layer 1'); //0 to 10000 is index of pixels that collied. Tile Layer 1 is what the layer is named in tiled map editor
   map.createLayer('background');
 
   layer = map.createLayer('Tile Layer 1');
+  layer = map.createLayer('Tile Layer 2');
   layer.resizeWorld();
 
   goombas = game.add.group();
